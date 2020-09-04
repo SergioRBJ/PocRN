d2ada5837a00c12bc25d611ae0ca8b2de51e5d03
Sergio Ricardo Bernardi Junior <sjunior@lnxcit018766.cit>
2020-09-04 14:00:14 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,59 @@
+
+import React from 'react';
+import { WebView } from 'react-native-webview';
+import {
+  StyleSheet
+} from 'react-native';
+
+import {
+  Colors
+} from 'react-native/Libraries/NewAppScreen';
+
+declare const global: { HermesInternal: null | {} };
+
+const App = () => {
+  return (
+          <WebView source={{ uri: 'http://estaciodocente.webaula.com.br/cursos/EX0031/aula2.html' }} />
+  );
+};
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+});
+
+export default App;
---
+++ HtmlContent.ts
@@ -0,0 +1,204 @@
+export const htmlContent = `
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+	<!--< Reset dom-->
+	<link rel="stylesheet" href="../repositorio_graduacao/v1/css/reset.css" />
+
+	<!--< Framework -->
+	<link rel="stylesheet" href="../repositorio_graduacao/v1/css/bootstrap/bootstrap.min.css" media="screen" />
+
+
+	<!--< Ícones -->
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU"
+	 crossorigin="anonymous">
+
+	<!--< Estrutura -->
+	<link rel="stylesheet" href="../repositorio_graduacao/v1/css/estrutura.css" title="1" media="screen" />
+	<link rel="stylesheet" href="../repositorio_graduacao/v1/css/responsivo.css" title="1" media="screen" />
+
+	<!--< Acessibilidade -->
+	<link rel="alternate stylesheet" href="../repositorio_graduacao/v1/css/estilo-altocontraste.css" title="2" media="screen" />
+
+	<!--< Customizável-->
+	<link rel="stylesheet" href="" />
+
+	<!--< Impressão -->
+	<link rel="stylesheet" type="text/css" href="../repositorio_graduacao/v1/css/print.css" media="print" />
+
+
+	<!-- Fórmulas Matemáticas -->
+	<script type="text/javascript" src="//www.hostmath.com/Math/MathJax.js?config=OK"></script>
+
+	<title>Estácio - Disciplina online</title>
+</head>
+
+<!-- Imagem abertura -->
+<div class="img_abertura d-print-none mdc-bg-grey-900" style="background: url(galeria/apresentacao/img/abertura.jpg) no-repeat center top"
+ role="img" aria-label="Imagem de abertura da aula"></div>
+<div class="img_abertura2 d-none d-print-block" style="background: url(//estacio.webaula.com.br/Cursos/repositorio_graduacao/v1/img/print_bg.jpg) no-repeat center bottom"
+ role="img" aria-hidden="true"></div>
+
+<!-- Apresentacao/objetivos -->
+<div class="container-fluid pt-5" id="box-apresentacao-objetivos">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 my-4">
+                <h3 class="mb-4">Apresentação</h3>
+
+                <p>Nesta aula, apresentaremos os aspectos que distinguem as regiões e as influências recebidas de países europeus
+                    e asiáticos na construção da gastronomia regional brasileira.
+                </p>
+
+
+
+
+            </div>
+        </div>
+        <hr aria-hidden="true">
+        <div class="row">
+            <div class="col-12 my-5">
+                <h3 class="mb-4">Objetivos</h3>
+                <ul>
+                    <li class="mb-3">Apontar a evolução histórica da gastronomia brasileira;</li>
+                    <li class="mb-3">Distinguir a gastronomia por regiões do Brasil;</li>
+                    <li class="mb-3">Reconhecer influências dos imigrantes na gastronomia regional brasileira.</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+</header>
+
+<!-- Inicio conteudo aula -->
+<section id="conteudo" class="conteudo">
+
+
+
+
+<!-- BLOCO TEXTO -->
+<div class='container mt-5' id='topico-01'>
+    <div class='row'>
+        <div id='' class='col-12'>
+            <h1 class='titulo mb-2'>Evolução e formação da gastronomia brasileira</h1>
+        </div>
+    </div>
+</div>
+<!-- FIM BLOCO DE TEXTO -->
+
+
+
+
+
+
+
+<!-- Recurso texto com imagem a direita-->
+<div class="container-fluid py-5">
+    <div class="container">
+        <div class="row align-items-start">
+            <div class="col-12 col-sm-6 col-lg-6">
+                <p>Repleta de influências, a cozinha brasileira começou com a mistura entre as culturas indígena, portuguesa e
+                    africana. Segundo Freixa e Chaves (2012, p. 177), a gastronomia brasileira começou a se consolidar a partir do
+                    século XVII, com a implementação dos engenhos de <span class="mdc-text-yellow-900" style="font-size:20px"><strong>cana-de-açúcar</strong></span>
+                    no Nordeste.
+                </p>
+
+                <p>Este é um fator importante, mas o aspecto determinante para esta consolidação foi a chegada das famílias
+                    portuguesas, que vieram com o objetivo de se estabelecerem e constituírem uma sociedade. A presença do núcleo
+                    familiar organizado, sob a <a href="" class="link-modal mdc-bg-blue-100 mdc-text-blue-900 p-1 " data-toggle="modal"
+                     data-target="#modal-conteudo-01">administração da mulher portuguesa <sup>1</sup></a>, foi fundamental para essa
+                    consolidação.
+                </p>
+
+            </div>
+            <div class="col-12 col-sm-6 col-lg-6" aria-hidden="true">
+                <div class="row">
+                    <div class="col-12 order-12">
+                        <div class="legenda mt-2">
+                            <div>
+                                <i class="material-icons">&#xE412;</i>
+                                <span>Fonte: Por cgdeaw (Adaptado) / Shutterstock.</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 order-1 ">
+                        <img src="galeria/aula2/img/img01.jpg" alt="" class="img-fluid ">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+<!-- VÍDEO VIMEO -->
+<div class='container recursoVideo my-4'>
+<div class='row'>
+<div class='col-12 d-print-none'>
+    <div class='embed-responsive embed-responsive-16by9'>
+        <iframe class='embed-responsive-item' src="https://player.vimeo.com/video/319041991/?title=0&byline=0&portrait=0&badge=0&autopause=0&player_id=0&app_id=141351" width="1280" height="720" frameborder="0" title="" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+    </div>
+</div>
+</div>
+</div>
+<!-- VÍDEO VIMEO -->		
+
+
+
+
+
+<!-- Recurso texto com imagem a esquerda-->
+<div class="container-fluid py-5">
+    <div class="container">
+        <div class="row align-items-start">
+            <div class="col-12 col-sm-6" aria-hidden="true">
+                <div class="row" data-aos="fade-right" data-aos-duration="4000">
+                    <div class="col-12 order-12">
+                        <div class="legenda mt-2">
+                            <div class=""><i class="material-icons">&#xE412;</i>
+                                <span>Fonte: Por Anna Kucherova (Adaptado) / Shutterstock.</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 order-1 ">
+                        <img src="galeria/aula2/img/img02.jpg" alt="" class="img-fluid ">
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12 col-sm-6">
+                <p>As receitas foram trazidas pelas mulheres portuguesas, preparadas e adaptadas com os ingredientes locais, com
+                    ajuda da mulher africana, que muitas vezes fez suas intervenções criativas com sucesso. As receitas, que serão
+                    abordadas nas aulas sobre a culinária da região Nordeste, giravam em torno do uso do açúcar, produzido em
+                    abundância, da mandioca e das <span class="mdc-text-yellow-900" style="font-size:20px"><strong>frutas tropicais</strong></span>.
+                    No litoral, concentrava-se a maior parte da população por conta
+                    das lavouras de cana-de-açúcar.
+                </p>
+
+                <p>Um dos fatores de ocupação do interior foi a expansão da pecuária, inicialmente de subsistência e que servia
+                    como força de trabalho nos engenhos, mas depois passou a ser usada também como estratégia de domínio das terras.
+                    Os tropeiros do Nordeste levavam carne seca, rapadura e queijo coalho para se alimentar durante a viagem.
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+<!-- VÍDEO VIMEO -->
+<div class='container recursoVideo my-4'>
+<div class='row'>
+<div class='col-12 d-print-none'>
+    <div class='embed-responsive embed-responsive-16by9'>
+        <iframe class='embed-responsive-item' src="https://player.vimeo.com/video/319041646/?title=0&byline=0&portrait=0&badge=0&autopause=0&player_id=0&app_id=141351" width="1280" height="720" frameborder="0" title="" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+    </div>
+</div>
+</div>
+</div>
+<!-- VÍDEO VIMEO -->		
+`
+;
